95a68a682babcaa6696088cad805729440e1ad9d
sanjeetsunit <sanjeetsunit@gmail.com>
2022-04-25 08:50:34 +0530

    fixing teh code

--- multiply.java
+++ multiply.java
@@ -1,5 +1,6 @@
 public class multiply {
     public static void main(String[] args){
-        System.out.println("This Coded for new branch create Multiply ")
+
+        System.out.println("This Coded for new branch create Multiply ");
     }
 }
