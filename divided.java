015f96199b5d9e33d1d1824f9b6d0ce793908529
sanjeetsunit <sanjeetsunit@gmail.com>
2022-04-25 09:09:19 +0530

    divided feature added

---
+++ divided.java
@@ -0,0 +1,7 @@
+public class dicided {
+
+    public static void main(String[] args){
+        System.out.println("Yes i added for divided");
+        System.out.println("yes ia dded new change");
+    }
+}
