d32dd1e546373df5a0e55c401de35ab7a7945326
sanjeetsunit <sanjeetsunit@gmail.com>
2022-04-25 17:19:31 +0530

    HashMap-Code

---
+++ HashMap.java
@@ -0,0 +1,12 @@
+public class HashMap {
+
+    public static void main(String[] args){
+
+        Map<String, Integer> map=new HashMap();
+        map.put("Sanjeet",1);//null key supported
+        map.put("Sunit",2);
+
+        System.out.println("The HashMap "+map);
+
+    }
+}
