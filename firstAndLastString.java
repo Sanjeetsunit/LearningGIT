0fdfd03e5f46b750727f950ab5f41d8a044623a1
sanjeetsunit <sanjeetsunit@gmail.com>
2022-04-25 16:25:37 +0530

    First&LastString

--- firstAndLastString.java
+++ firstAndLastString.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class firstAndLastString {
+public class firstAndLastString {
+    public static void main(String[] args){
+
+        String str1="Sanjeet kumar singh";
+        String first= str1.indexOf(" ");
+        String firstspace=str1.substring(0,first);
+
+        String last =str1.substring(str1.lastIndexOf(" "));
+
+        System.out.println("The first string is ::"+firstspace);
+        System.out.println("The last STring is ::"+last);
+
+        )
+    }
 }
