ad9a06a46b30af1b1b8618437b62404e3385fa22
sanjeetsunit <sanjeetsunit@gmail.com>
2022-04-27 09:22:53 +0530

    OverRide Code Adding

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,14 @@
 public class HelloWorld {
 
+    public static int getadding(int a,int b){
+        return a+b ;
+    }
+    public static int getadding(int a,int b,int c){
+        return a+b+c;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World of INdia ");
+
+        System.out.println(getadding(2,5));
+        System..out.println(getadding(1,9,4))
     }
 }
