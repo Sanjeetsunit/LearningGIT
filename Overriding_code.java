1d66e5364e6f6079625f09daa7f207f4d5f32599
sanjeetsunit <sanjeetsunit@gmail.com>
2022-04-27 09:15:58 +0530

    yes

---
+++ Overriding_code.java
@@ -0,0 +1,12 @@
+class Overriding_code {
+    public static int multiply(int a,int b){
+        return a*b;
+
+    }
+
+    public static void main(String[] args){
+
+        System.out.println(multiply(2,3));
+    }
+
+}
--- multiply.java
+++
@@ -1,6 +0,0 @@
-public class multiply {
-    public static void main(String[] args){
-
-        System.out.println("This Coded for new branch create Multiply ");
-    }
-}
